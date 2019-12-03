fd6a4f8498443ab40eb5957f9d6636e21a1d48fd
Craig Carr <chaoxys@gmail.com>
2019-12-03 04:56:34 -0500

    Added installation indicator; added placeholder language selection

--- src/Pages/SettingsPage/SettingsContent/SettingsContent.tsx
+++ src/Pages/SettingsPage/SettingsContent/SettingsContent.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Table, Checkbox } from 'semantic-ui-react';
+import { Table, Checkbox, Select } from 'semantic-ui-react';
 import styles from './SettingsContent.module.scss';
 import { SettingsContext } from '../../../App';
 import TooltipButton from '../../../Components/TooltipButton/TooltipButton';
@@ -66,10 +66,51 @@ export default function SettingsContent() {
     setDarkModeEnabled(!isDarkModeEnabled);
   }
 
+  const languageOptions = [
+    { key: 'en', value: 'en', text: 'English' },
+    { key: 'de', value: 'de', text: 'German' },
+  ]
+
+  let isInstalledText = null;
+  let isInstalledColor = null;
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    isInstalledText = "Installed";
+    isInstalledColor = "#ddddff";
+  } else {
+    isInstalledText = "Not Installed";
+    isInstalledColor = "#ffdddd";
+  }
+
   return (
     <div className={styles.content}>
       <Table unstackable columns={3}>
         <Table.Body>
+          <Table.Row style={{ backgroundColor: isInstalledColor }}>
+            <Table.Cell>
+              Installation Status
+            </Table.Cell>
+            <Table.Cell>
+              <TooltipButton text="Add CountLog to your home screen for a more authentic app experience." />
+            </Table.Cell>
+            <Table.Cell>
+              {isInstalledText}
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell className={styles.tableCell}>
+              <p>Language</p>
+            </Table.Cell>
+            <Table.Cell>
+              <TooltipButton text="Change the language for all text in the app." />
+            </Table.Cell>
+            <Table.Cell>
+              <Select
+                className={styles.select}
+                defaultValue="en"
+                options={languageOptions} />
+              <p>Not yet supported!</p>
+            </Table.Cell>
+          </Table.Row>
           <Table.Row>
             <Table.Cell className={styles.tableCell}>
               <p>Dark Mode</p>
