c75c670db0504b8d695c7df489bbfce8bebff988
Craig Carr <chaoxys@gmail.com>
2019-12-06 14:33:27 -0500

    Testing PWA install behavior with code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import MainPage from './Pages/MainPage/MainPage';
-import AboutPage from './Pages/AboutPage/AboutPage';
-import SettingsPage from './Pages/SettingsPage/SettingsPage';
-import CounterCreatePage from './Pages/CounterCreatePage/CounterCreatePage';
+
 import CounterDatabase from './CounterDatabase';
-import CounterEditPage from './Pages/CounterEditPage/CounterEditPage';
-import StatisticsPage from './Pages/StatisticsPage/StatisticsPage';
-import CounterDeletePage from './Pages/CounterDeletePage/CounterDeletePage';
-import CounterHistoryPage from './Pages/CounterHistoryPage/CounterHistoryPage';
-import EventEditPage from './Pages/EventEditPage/EventEditPage';
 import LoggingAPI from './Interfaces/LoggingAPI';
 import CountersAPI from './Interfaces/CountersAPI';
 import EventsAPI from './Interfaces/EventsAPI';
 import SettingsAPI from './Interfaces/SettingsAPI';
-import './App.scss';
-import ReceiverListPage from './Pages/ReceiverListPage/ReceiverListPage';
-import ReceiverEditPage from './Pages/ReceiverEditPage/ReceiverEditPage';
-import ReceiverCreatePage from './Pages/ReceiverCreatePage/ReceiverCreatePage';
-import ReceiverDeletePage from './Pages/ReceiverDeletePage/ReceiverDeletePage';
 import ReceiversAPI from './Interfaces/ReceiversAPI';
+import './App.scss';
+
+const MainPage = lazy(() => import('./Pages/MainPage/MainPage'));
+const AboutPage = lazy(() => import('./Pages/AboutPage/AboutPage'));
+const SettingsPage = lazy(() => import('./Pages/SettingsPage/SettingsPage'));
+const CounterCreatePage = lazy(() => import('./Pages/CounterCreatePage/CounterCreatePage'));
+const CounterEditPage = lazy(() => import('./Pages/CounterEditPage/CounterEditPage'));
+const StatisticsPage = lazy(() => import('./Pages/StatisticsPage/StatisticsPage'));
+const CounterDeletePage = lazy(() => import('./Pages/CounterDeletePage/CounterDeletePage'));
+const CounterHistoryPage = lazy(() => import('./Pages/CounterHistoryPage/CounterHistoryPage'));
+const EventEditPage = lazy(() => import('./Pages/EventEditPage/EventEditPage'));
+const ReceiverListPage = lazy(() => import('./Pages/ReceiverListPage/ReceiverListPage'));
+const ReceiverEditPage = lazy(() => import('./Pages/ReceiverEditPage/ReceiverEditPage'));
+const ReceiverCreatePage = lazy(() => import('./Pages/ReceiverCreatePage/ReceiverCreatePage'));
+const ReceiverDeletePage = lazy(() => import('./Pages/ReceiverDeletePage/ReceiverDeletePage'));
 
 const db = new CounterDatabase();
 const loggingApi = new LoggingAPI();
@@ -42,50 +44,24 @@ export default function App() {
           <EventsContext.Provider value={eventsApi}>
             <ReceiversContext.Provider value={receiversApi}>
               <Router>
-                <Switch>
-                  <Route path="/about">
-                    <AboutPage></AboutPage>
-                  </Route>
-                  <Route path="/create">
-                    <CounterCreatePage></CounterCreatePage>
-                  </Route>
-                  <Route path="/editcounter/:counterId">
-                    <CounterEditPage></CounterEditPage>
-                  </Route>
-                  <Route path="/deletecounter/:counterId">
-                    <CounterDeletePage></CounterDeletePage>
-                  </Route>
-                  <Route path="/counterhistory/:counterId/editevent/:eventId">
-                    <EventEditPage></EventEditPage>
-                  </Route>
-                  <Route path="/counterhistory/:counterId">
-                    <CounterHistoryPage></CounterHistoryPage>
-                  </Route>
-                  <Route path="/edit/:counterId">
-                    <CounterEditPage></CounterEditPage>
-                  </Route>
-                  <Route path="/statistics/:counterId">
-                    <StatisticsPage></StatisticsPage>
-                  </Route>
-                  <Route path="/settings">
-                    <SettingsPage></SettingsPage>
-                  </Route>
-                  <Route path="/receiverlist">
-                    <ReceiverListPage></ReceiverListPage>
-                  </Route>
-                  <Route path="/receivercreate">
-                    <ReceiverCreatePage></ReceiverCreatePage>
-                  </Route>
-                  <Route path="/receiveredit/:receiverId">
-                    <ReceiverEditPage></ReceiverEditPage>
-                  </Route>
-                  <Route path="/receiverdelete/:receiverId">
-                    <ReceiverDeletePage></ReceiverDeletePage>
-                  </Route>
-                  <Route path="/">
-                    <MainPage></MainPage>
-                  </Route>
-                </Switch>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Switch>
+                    <Route path="/about" component={AboutPage} />
+                    <Route path="/create" component={CounterCreatePage} />
+                    <Route path="/editcounter/:counterId" component={CounterEditPage} />
+                    <Route path="/deletecounter/:counterId" component={CounterDeletePage} />
+                    <Route path="/counterhistory/:counterId/editevent/:eventId" component={EventEditPage} />
+                    <Route path="/counterhistory/:counterId" component={CounterHistoryPage} />
+                    <Route path="/edit/:counterId" component={CounterEditPage} />
+                    <Route path="/statistics/:counterId" component={StatisticsPage} />
+                    <Route path="/settings" component={SettingsPage} />
+                    <Route path="/receiverlist" component={ReceiverListPage} />
+                    <Route path="/receivercreate" component={ReceiverCreatePage} />
+                    <Route path="/receiveredit/:receiverId" component={ReceiverEditPage} />
+                    <Route path="/receiverdelete/:receiverId" component={ReceiverDeletePage} />
+                    <Route path="/" component={MainPage} />
+                  </Switch>
+                </Suspense>
               </Router>
             </ReceiversContext.Provider>
           </EventsContext.Provider>
